fc6c0c9c724562fdf96d5f3cd7ae96e639d7106a
Bryan Sanchez <bsanchezhtx@gmail.com>
2024-02-25 20:04:16 -0600

    add testcases

---
+++ setops.py
@@ -0,0 +1,8 @@
+import sys
+
+def main():
+    # getting command line input
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ testcases/run.sh
@@ -0,0 +1,4 @@
+python3 setops.py "set1=a1.txt;set2=b1.txt;operation=intersection"
+python3 setops.py "set1=a2.txt;set2=b2.txt;operation=difference"
+python3 setops.py "set1=a3.txt;set2=b3.txt;operation=union"
+python3 setops.py "set1=a4.txt;set2=b4.txt;operation=intersection"
